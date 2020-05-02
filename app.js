446b76c2f4d1eec3fb73f81547006b0593f96389
Regina Alyssa <reginaalyssa01809@gmail.com>
2020-05-02 17:44:34 +0800

    Setup event listeners to add item on click of add button or on keypress of Enter

--- app.js
+++ app.js
@@ -8,4 +8,20 @@ var UIController = (function () {
 
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var ctrlAddItem = function () {
+        // 1. Get field input data
+        // 2. Add item to budget controller
+        // 3. Add item to UI
+        // 4. Calculate the budget
+        // 5. Display the budget on the UI
+    };
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
+    });
 })(budgetController, UIController);
