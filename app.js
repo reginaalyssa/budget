4051caae8d47423130cdce05b7652af5427e282a
Regina Alyssa <reginaalyssa01809@gmail.com>
2020-05-02 19:22:51 +0800

    Create addItem function within budget controller

--- app.js
+++ app.js
@@ -19,7 +19,24 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        },
+        }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            data.allItems[type].push(newItem);
+            return newItem;
+        }
     };
 
 })();
